bbd5f3973d38c17c2dfc4c5bdce1e06865430727
Ivan Grebeniuk <ivan@moqod.com>
2019-02-17 21:03:01 +0400

    Добавлены тесты создания, удаления, модификации для групп и контактов.

---
+++ test/test_edit_contact.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(first_name="Jane", middle_name="Zzz", last_name="Aaa", nickname="Patric Star", title="Ms", company="Updated company", address="Bikini bottom, World Ocean", home_phone="000111",
+                               mobile_phone="000000", work_phone="000000", fax="00000", email="test@test.com", email2="test2@test.com",
+                               email3="test3@test.com", home_page="https:/google.com", b_day="1", b_month="January", b_year="2019", a_day="2", a_month="January", a_year="2019",
+                               secondary_address="Test secondary address", phone2="01", note="Hello world"))
+    app.session.logout()
