76221dedc002b5b9953d30012821c5887f8cbff3
Ivan Grebeniuk <ivan@moqod.com>
2019-02-18 14:37:44 +0400

    Добавлен вспомогательный метод fill_in_group_data

--- fixture/group.py
+++ fixture/group.py
@@ -7,12 +7,8 @@ class Grouphelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def fill_in_group_data(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill in group fields
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -22,6 +18,14 @@ class Grouphelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill in group fields
+        self.fill_in_group_data(group)
         # Submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -43,15 +47,7 @@ class Grouphelper:
         # submit editing group
         wd.find_element_by_name("edit").click()
         # update group data
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_in_group_data(group)
         # submit update
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,6 +3,6 @@ from model.group import Group
 
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Update name", header="Update group header", footer="Update group footer"))
+    app.group.edit_first_group(Group(name="Hello world", header="Update group header AGAIN", footer="Update group footer AGAIN"))
     app.session.logout()
 
