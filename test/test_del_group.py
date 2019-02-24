cbea964bc7624dae4a16e3653e34931bebd82e55
Ivan Grebeniuk <ivan@moqod.com>
2019-02-24 22:27:21 +0400

    Добавлены предусловия для тестов test_modify_group и test_del_group

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,8 @@
+from model.group import Group
 
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group added in DELETE test", header="Hello", footer="World"))
     app.group.delete_first_group()
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,8 +2,19 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.group.modify_first_group(Group(name="XXX"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group added in MODIFY test", header="Some test header", footer="Some test footer"))
+    app.group.modify_first_group(Group(name="Modified group name"))
 
 
 def test_modify_group_header(app):
-    app.group.modify_first_group(Group(header="XXX"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group added in MODIFY test", header="Some test header", footer="Some test footer"))
+    app.group.modify_first_group(Group(header="Modified group header"))
+
+
+def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group added in MODIFY test", header="Some test header", footer="Some test footer"))
+    app.group.modify_first_group(Group(footer="Modified group footer"))
+
