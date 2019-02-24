f5d36c6c1bb67d5bfce3fa3e40fa359dc8a4137b
Ivan Grebeniuk <ivan@moqod.com>
2019-02-24 12:03:43 +0400

    добавлено ветвление if/else в тест modify group

--- fixture/group.py
+++ fixture/group.py
@@ -7,17 +7,20 @@ class Grouphelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+        else:
+            pass
+
     def fill_in_group_data(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
     def create(self, group):
         wd = self.app.wd
@@ -39,11 +42,10 @@ class Grouphelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def edit_first_group(self, group):
+    def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit editing group
         wd.find_element_by_name("edit").click()
         # update group data
@@ -51,6 +53,10 @@ class Grouphelper:
         # submit update
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- test/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-from model.group import Group
-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Hello world", header="Update group header AGAIN", footer="Update group footer AGAIN"))
-    app.session.logout()
-
