39a4b39c5498a28cfd9a2b6d2077139ad0427d2a
Ivan Grebeniuk <ivan@moqod.com>
2019-02-24 13:06:21 +0400

    Добавлена валидация фикстуры перед каждым тестом, починены тесты test_edit_contact и test_del_contact

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="Hello world"))
-    app.session.logout()
+    app.group.modify_first_group(Group(name="XXX"))
 
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(header="Updated header"))
-    app.session.logout()
+    app.group.modify_first_group(Group(header="XXX"))
