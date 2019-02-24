020889a15ed8e9d3e468e944a91c0ec424149bfe
Ivan Grebeniuk <ivan@moqod.com>
2019-02-24 16:42:44 +0400

    Попытка сделать проверку состояния сессий

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="New Test name", header="New Test group header", footer="New test group footer"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
